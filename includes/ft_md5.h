ed2d499c15a3c605df88c970ded094922e8f9114
srossi42 <ross.savinien@gmail.com>
2019-07-29 16:52:10 +0200

    md5 ok

--- includes/ft_md5.h
+++ includes/ft_md5.h
@@ -13,18 +13,68 @@
 #ifndef FT_MD5_H
 # define FT_MD5_H
 
-typedef unsigned char md5_byte_t; /* 8-bit byte */
-typedef unsigned int md5_word_t; /* 32-bit word */
+# define MD5_A		    md5_struct->abcdfg[0]
+# define MD5_B		    md5_struct->abcdfg[1]
+# define MD5_C		    md5_struct->abcdfg[2]
+# define MD5_D		    md5_struct->abcdfg[3]
+# define MD5_F		    md5_struct->abcdfg[4]
+# define MD5_G		    md5_struct->abcdfg[5]
+
+# define MD5_H0		    md5_struct->state[0]
+# define MD5_H1		    md5_struct->state[1]
+# define MD5_H2		    md5_struct->state[2]
+# define MD5_H3		    md5_struct->state[3]
+
+static uint32_t	g_md5_r[64] = {
+        7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
+        5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20,
+        4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
+        6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
+};
+
+static uint32_t	g_md5_k[64] = {
+        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+        0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+        0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+        0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+        0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+        0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+        0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+        0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+        0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+        0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+        0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+        0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+        0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+        0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+};
+
+typedef unsigned char   md5_byte_t; /* 8-bit byte */
+typedef unsigned int    md5_word_t; /* 32-bit word */
 
 /* Contexte MD5. */
-typedef struct  s_md5_state{
-    md5_word_t state[4]; /* digest buffer (ABCD) */
-    md5_word_t count[2]; /* message length in bits, lsw first */
-    md5_byte_t buffer[64]; /* mémoire tampon d'entrée */
-}               t_md5_state;
+typedef struct      s_md5_struct{
+
+    md5_word_t      state[4]; /* digest buffer (ABCD) */
+    md5_word_t      abcdfg[6];
+    md5_word_t      count[2]; /* message length in bits, lsw first */
+    md5_byte_t      buffer[64]; /* mémoire tampon d'entrée */
+    char            *input;
+    unsigned char   *data;
+    size_t          input_len;
+    size_t          total_len;
+
+}                   t_md5_struct;
 
 
+
+void    ft_md5_init(t_md5_struct *md5_struct, char *str_to_hash);
+void    ft_md5_free(t_md5_struct *md5_struct);
 void    ft_md5(char *str_to_hash);
-void    ft_md5_init(t_md5_state *md5_state);
+void    ft_md5_encode(t_md5_struct *md5_struct);
+void    ft_md5_padding(t_md5_struct *md5_struct);
+void    ft_md5_append_len(t_md5_struct *md5_struct);
 
 #endif
--- srcs/md5.c
+++ srcs/md5.c
@@ -7,128 +7,159 @@
 
 #include <stdio.h>
 
-/* Initialisation de l'état (ABCD) et du nb de bits*/
-
-void ft_md5_init (t_md5_state *md5){
-    md5->count[0] = 0;
-    md5->count[1] = 0;
-    md5->state[0] = 0x67452301;
-    md5->state[1] = 0xefcdab89;
-    md5->state[2] = 0x98badcfe;
-    md5->state[3] = 0x10325476;
+uint32_t	ft_rotate_left(uint32_t x, uint32_t n)
+{
+    return ((x << n) | (x >> (32 - n)));
 }
 
-
-/*
-void ft_f(x, y, z){
-    (((x) & (y)) | ((~x) & (z)));
+uint32_t ft_reverse_bits(uint32_t value){
+    return (value & 0xFF000000)>>24| (value & 0x00FF0000)>>8
+    | (value & 0x0000FF00)<<8 | (value & 0x000000FF)<<24;
 }
 
-void ft_g(x, y, z){
-    (((x) & (z)) | ((y) & (~z)));
+void ft_md5_init(t_md5_struct *md5_struct, char *str_to_hash){
+    md5_struct->input = ft_strdup(str_to_hash);
+    md5_struct->input_len = ft_strlen(str_to_hash);
+    md5_struct->total_len = md5_struct->input_len + 1;
+
+    md5_struct->state[0] = 0x67452301;
+    md5_struct->state[1] = 0xefcdab89;
+    md5_struct->state[2] = 0x98badcfe;
+    md5_struct->state[3] = 0x10325476;
 }
 
-void ft_h(x, y, z){
-    ((x) ^ (y) ^ (z));
-}
-
-void ft_i(x, y, z){
-    ((y) ^ ((x) | (~z)));
-}
-*/
-/* ROTATE_LEFT fait une rotation de x de n bits à gauche. */
-/*void ft_rotate_left(x, n){
-    (((x) << (n)) | ((x) >> (32-(n))));
-}*/
-
-
-static int ft_convert(char *text){
-    int len = strlen(text);
-
-    char hex[100], string[50];
-
-    // Convert text to hex.
-    for (int i = 0, j = 0; i < len; ++i, j += 2)
-        sprintf(hex + j, "%02x", text[i] & 0xff);
-
-    printf("'%s' in hex is %s.\n", text, hex);
-
-    // Convert the hex back to a string.
-    len = strlen(hex);
-    for (int i = 0, j = 0; j < len; ++i, j += 2) {
-        int val[1];
-        sscanf(hex + j, "%2x", val);
-        string[i] = val[0];
-        string[i + 1] = '\0';
+void ft_md5_free(t_md5_struct *md5_struct) {
+    if (md5_struct->data){
+        ft_bzero(md5_struct->data, md5_struct->total_len);
+        free(md5_struct->data);
     }
+    if (md5_struct->input)
+        ft_strdel(&md5_struct->input);
+}
 
-    printf("%s as a string is '%s'.\n", hex, string);
+void ft_md5_padding(t_md5_struct *md5_struct) {
+    unsigned int    bitlen;
+    int             msg_len;
+    int             padding;
 
-    return 0;
+    padding = 0;
+    msg_len = md5_struct->input_len;
+    bitlen = md5_struct->input_len<<3;
+    md5_struct->count[1] = md5_struct->input_len >> 29;
+    md5_struct->count[0] = bitlen;
+    while (((md5_struct->input_len + padding + 1) * 8 - 448) % 512 != 0)
+        padding++;
+    md5_struct->total_len += padding;
+    md5_struct->data = (unsigned char *) ft_strnew(md5_struct->total_len + 8);
+    ft_memcpy(md5_struct->data, md5_struct->input, md5_struct->input_len);
+    md5_struct->data[msg_len] = (unsigned char) 128;
+    while (++msg_len <= md5_struct->total_len)
+        md5_struct->data[msg_len] = 0;
+    ft_memcpy(md5_struct->data + md5_struct->total_len, &bitlen, 4);
+}
+
+void ft_init_abcd(t_md5_struct *md5_struct){
+    MD5_A = MD5_H0;
+    MD5_B = MD5_H1;
+    MD5_C = MD5_H2;
+    MD5_D = MD5_H3;
+}
+
+void ft_update_abcd(t_md5_struct *md5_struct, md5_byte_t word[4], int j){
+    int         tmp;
+    uint32_t    w_g;
+
+    w_g = (uint32_t)word[0] | (uint32_t)(word[1]<<8) | (uint32_t)(word[2]<<16) | (uint32_t)(word[3]<<24);
+    tmp = MD5_D;
+    MD5_D = MD5_C;
+    MD5_C = MD5_B;
+    MD5_B += ft_rotate_left(MD5_A + MD5_F + g_md5_k[j] + w_g, g_md5_r[j]);
+    MD5_A = tmp;
+}
+
+void ft_update_hx(t_md5_struct *md5_struct){
+    MD5_H0 += MD5_A;
+    MD5_H1 += MD5_B;
+    MD5_H2 += MD5_C;
+    MD5_H3 += MD5_D;
+}
+
+void ft_get_fg(t_md5_struct *md5_struct, int j){
+    if (j < 16) {
+        MD5_F = (MD5_B & MD5_C) | ((~MD5_B) & MD5_D);
+        MD5_G = j;
+    } else if (j < 32) {
+        MD5_F = (MD5_D & MD5_B) | ((~MD5_D) & MD5_C);
+        MD5_G = (5 * j + 1) % 16;
+    } else if (j < 48) {
+        MD5_F = MD5_B ^ MD5_C ^ MD5_D;
+        MD5_G = (3 * j + 5) % 16;
+    } else if (j < 64) {
+        MD5_F = MD5_C ^ (MD5_B | (~MD5_D));
+        MD5_G = (7 * j) % 16;
+    }
+}
+
+void ft_md5_encode(t_md5_struct *md5_struct) {
+    int         i;
+    int         j;
+    md5_byte_t  word[4];
+    md5_byte_t  words[64];
+
+    ft_bzero(word, 4);
+    //i = 1;
+    i = 0;
+    md5_struct->total_len += 8;
+    //while (i < md5_struct->total_len) {
+    while (i < md5_struct->total_len - 1) {
+        ft_bzero(words, 64);
+        //i--;
+        ft_memcpy(words, md5_struct->data + i, 64);
+        //if (i > 0)
+          //  i++;
+        ft_init_abcd(md5_struct);
+        j = 0;
+        while (j < 64) {
+            ft_get_fg(md5_struct, j);
+            ft_memcpy(word, words + MD5_G * 4, 4);
+            ft_update_abcd(md5_struct, word, j++);
+        }
+        ft_update_hx(md5_struct);
+        i += 64;
+    }
+}
+
+
+void ft_reverse_words(t_md5_struct *md5_struct){
+    MD5_H0 = ft_reverse_bits(MD5_H0);
+    MD5_H1 = ft_reverse_bits(MD5_H1);
+    MD5_H2 = ft_reverse_bits(MD5_H2);
+    MD5_H3 = ft_reverse_bits(MD5_H3);
+}
+
+char *ft_md5_decode(t_md5_struct *md5_struct){
+    char *res;
+
+    ft_reverse_words(md5_struct);
+    res = ft_itoa_base_ull(MD5_H0, "0123456789abcdef");
+    res = ft_strjoin(res, ft_itoa_base_ull(MD5_H1, "0123456789abcdef"));
+    res = ft_strjoin(res, ft_itoa_base_ull(MD5_H2, "0123456789abcdef"));
+    res = ft_strjoin(res, ft_itoa_base_ull(MD5_H3, "0123456789abcdef"));
+    return res;
 }
 
 
 void ft_md5(char *str_to_hash){
-    int msg_len;
-    int padding;
-    unsigned char *str_pad;
-    int total_len;
+    struct s_md5_struct md5_struct;
+    char *res;
 
-
-    ft_printf("ft_md5\n");
-
-    ft_printf("chaine avant padding : %s\n", str_to_hash);
-    ft_convert(str_to_hash);
-    msg_len = ft_strlen(str_to_hash);
-
-    padding = msg_len;
-    while ((padding + msg_len) % 56 != 0)
-        padding++;
-
-    total_len = msg_len + padding + 8;
-
-    ft_printf("msg_len : %d\n", msg_len);
-    ft_printf("padding : %d\n", padding);
-    ft_printf("total len : %d\n", total_len);
-
-    str_pad = (unsigned char*)ft_strnew(msg_len + padding + 8);
-    ft_memcpy(str_pad, str_to_hash, msg_len);
-    str_pad[msg_len] = (char)128;
-    while (++msg_len <= (total_len - 8))
-    {
-        str_pad[msg_len] = 0;
-        ft_printf("msg len : %d\n", msg_len);
-    }
-
-    int i = 0;
-    ft_printf("msg original en b :\n");
-    while (i < msg_len)
-    {
-        ft_printf("%b ", (char)str_to_hash[i]);
-        if ((i + 1) % 16 == 0)
-            ft_printf("\n");
-        i++;
-    }
-    ft_printf("\n");
-    i = 0;
-    ft_printf("msg padded en b :\n");
-    while (i < total_len)
-    {
-        ft_printf("%b ", (char)str_pad[i]);
-        if ((i + 1) % 16 == 0)
-            ft_printf("\n");
-        i++;
-    }
-    ft_printf("\n");
-
-/*    while (i < msg_len / 8){
-        ft_printf("char : %c\n", str_to_hash[i]);
-        str_pad[i] = str_to_hash[i];
-        i++;
-    }*/
-    ft_printf("chaine apres padding : %s\n", str_to_hash);
-    ft_printf("chaineb apre padding : %s\n", str_pad);
-    //ft_printf("msgLen (bits) = %d\n", msg_len);
-    //ft_printf("msgLen (bits) = %d\n", msg_len);
-    //ft_printf("msgLen (bits) = %d\n", msg_len);
+    ft_md5_init(&md5_struct, str_to_hash);
+    ft_md5_padding(&md5_struct);
+    ft_md5_encode(&md5_struct);
+    res = ft_md5_decode(&md5_struct);
+    //mettre le res dans la structure pour éviter de malloc à chaque fois une string
+    printf("MD5 (\"%s\") = %s\n", md5_struct.input, res);
+    //ft_md5_print_res(&md5_struct);
+    ft_md5_free(&md5_struct);
+    //return res;
 }
